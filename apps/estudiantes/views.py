5f722cebe1c42a187fe8ecd393b8c78e778a78d5
INGJEISSON <jeisim18@gmail.com>
2025-03-06 16:59:01 -0500

    Update views.py

--- apps/estudiantes/views.py
+++ apps/estudiantes/views.py
@@ -1,12 +1,12 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Estudiante
 from .serializers import EstudianteSerializer
 
 class EstudianteViewSet(viewsets.ModelViewSet):
     queryset = Estudiante.objects.all()
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     serializer_class = EstudianteSerializer
-    filterset_fields = ('__all__')  # Reemplaza 'campo1', 'campo2' por los nombres de los campos que deseas filtrar
-    search_fields = ('__all__')       # Reemplaza 'campo1', 'campo2' por los campos en los que se realizará la búsqueda
-    ordering_fields = ('__all__')     # Si deseas habilitar el ordenamiento
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = '__all__'  # Permite filtrar por todos los campos del modelo
+    search_fields = '__all__'     # Permite buscar en todos los campos (si es compatible con tu configuración)
+    ordering_fields = '__all__'   # O bien, lista explícitamente los campos, por ejemplo: ['campo1', 'campo2']
