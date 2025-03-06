5e307ba5d4b9bd1080bf896d9863708521bd6dbf
INGJEISSON <jeisim18@gmail.com>
2025-03-06 13:30:30 -0500

    fix

--- sisvoteAdmin/settings.py
+++ sisvoteAdmin/settings.py
@@ -1,7 +1,5 @@
 import os
-import pymysql
 from pathlib import Path
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
