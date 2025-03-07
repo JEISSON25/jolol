83a158685c3f8411b433e9401c3e8106f96d3cc8
INGJEISSON <jeisim18@gmail.com>
2025-03-06 22:40:56 -0500

    fix model voto

--- apps/candidatos/admin.py
+++ apps/candidatos/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from import_export.admin import ImportExportModelAdmin
-from .models import Candidato, TipoEleccion
+from .models import Candidato, TipoEleccion, Voto
 
+@admin.register(Voto)
+class VotoAdmin(ImportExportModelAdmin):
+    list_display = ('estudiante', 'mesa', 'empresa', 'fechaHora')
+    
 @admin.register(Candidato)
 class CandidatoAdmin(ImportExportModelAdmin):
     list_display = ('nombre', 'eleccion', 'empresa', 'tarjeton' ,'votos')
+
 
 
 @admin.register(TipoEleccion)
--- apps/candidatos/models.py
+++ apps/candidatos/models.py
@@ -33,8 +33,7 @@ class Candidato(TimeStampedModel):
 
 class Voto(TimeStampedModel):
     estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE)
-    candidato1 = models.ForeignKey(Candidato, related_name='votos_candidato1', on_delete=models.CASCADE)
-    candidato2 = models.ForeignKey(Candidato, related_name='votos_candidato2', on_delete=models.CASCADE)
+    candidato = models.ForeignKey(Candidato, related_name='votos_candidato', on_delete=models.CASCADE)
     fechaHora = models.DateTimeField(auto_now_add=True)
     mesa = models.ForeignKey(Mesa, on_delete=models.CASCADE)
     empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
